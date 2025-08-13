eb4c5a09b65dddce32f43e3d788f6fb5c7abea3b
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-08-13 06:36:38 -0300

    Prompt template

--- categorizador.py
+++ categorizador.py
@@ -13,27 +13,48 @@ client = anthropic.Anthropic(
 modelo = "claude-3-5-sonnet-20240620"
 # modelo = "claude-3-5-haiku-20240307"
 
-prompt_do_sistema = "Classifique o alimento abaixo em uma das categorias: Bebida, Comida Salgada e Comida Doce. Dê uma descrição da categoria."
+def categoriza_alimento(lista_categorias_validas, nome_do_alimento):
+  prompt_do_sistema = f"""
+  Você é um categorizador de alimentos.
+  Você não deve responder outros objetos que não sejam alimentos.
+  Você deve assumir as categorias presentes na lista abaixo:
 
-prompt_do_usuario = input("Digite um alimento: ")
+  # Lista de Categorias Válidas
+  {lista_categorias_validas.split(",")}
 
-message = client.messages.create(
-  model=modelo,
-  max_tokens=1000,
-  temperature=0,
-  system=prompt_do_sistema,
-  messages = [
-    {
-      "role": "user",
-      "content": [
-        {
-          "type": "text",
-          "text": prompt_do_usuario
-        }
-      ]
-    }
-  ]
-)
+  # Formato da Saída
+  Produto: Nome do Produto
+  Categoria: Apresente a categoria do produto
 
-resposta = message.content[0].text
-print(resposta)
+  # Exemplo de Saída
+  Produto: Maçã
+  Categoria: Frutas
+  """
+  prompt_do_usuario = nome_do_alimento
+  message = client.messages.create(
+    model=modelo,
+    max_tokens=1000,
+    temperature=0,
+    system=prompt_do_sistema,
+    messages = [
+      {
+        "role": "user",
+        "content": [
+          {
+            "type": "text",
+            "text": prompt_do_usuario
+          }
+        ]
+      }
+    ]
+  )
+
+  resposta = message.content[0].text
+  return resposta
+
+
+categorias_validas = input("Informe as categorias válidas, separando por vírgula: ")
+while True:
+  nome_do_alimento = input("Digite o nome do alimento: ")
+  texto_resposta = categoriza_alimento(categorias_validas, nome_do_alimento)
+  print(texto_resposta)
