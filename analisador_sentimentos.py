425fb5580ff3a1a25c787572e67e26f4a1b429ab
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-08-14 13:41:09 -0300

    Tratamento de erros

--- analisador_sentimentos.py
+++ analisador_sentimentos.py
@@ -33,27 +33,37 @@ def analisador_sentimentos(restaurante):
 
   print(f'Iniciou a análise do {restaurante}')
 
-  mensagem = client.messages.create(
-    model=modelo,
-    max_tokens=2000,
-    temperature=0,
-    system=prompt_do_sistema,
-    messages = [
-      {
-        "role": "user",
-        "content": [
-          {
-            "type": "text",
-            "text": prompt_do_usuario
-          }
-        ]
-      }
-    ]
-  )
+  try:
+    mensagem = client.messages.create(
+      model=modelo,
+      max_tokens=2000,
+      temperature=0,
+      system=prompt_do_sistema,
+      messages = [
+        {
+          "role": "user",
+          "content": [
+            {
+              "type": "text",
+              "text": prompt_do_usuario
+            }
+          ]
+        }
+      ]
+    )
 
-  resposta = mensagem.content[0].text
-  print(f'Finalizou a análise do {restaurante}')
-  salva(f'./dados/analise/analise-{restaurante}.txt', resposta)
+    resposta = mensagem.content[0].text
+    print(f'Finalizou a análise do {restaurante}')
+    salva(f'./dados/analise/analise-{restaurante}.txt', resposta)
+
+  except anthropic.APIConnectionError as e:
+    print("O servidor não pode ser acessado! Erro: ", e.__cause__)
+  except anthropic.RateLimitError as e:
+    print("Um status code 429 foi recebido! Limite de acesso foi atingido.")
+  except anthropic.APIStatusError as e:
+    print(f"Um erro {e.status_code} foi recebido. Mais informações: {e.response}")
+  except Exception as e:
+    print(f"Um erro inesperado ocorreu: {e}")
 
 
-analisador_sentimentos('bolos-doces')
+analisador_sentimentos('comida-vegana')
